da22efd939adeb2d41b939e2558dd1c8ba316f80
enix426 <58696683+enix426@users.noreply.github.com>
2021-11-04 01:29:45 -0400

    form avec ajax

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,37 +1,47 @@
-(function() {
-  
+(function(){
+  var contactForm = $('#js-contact');
+  var contactMessage = document.getElementById('message');
+  var contactSubmit = $('#js-contact-submit');
+
+  contactSubmit.prop('disabled', true);
+
+  contactForm.submit(onContactSubmit);
+  contactMessage.addEventListener('input', onContactInput);
+  $('.js-contact-success').hide();
+  $('.js-contact-error').hide();
+
   function onContactSubmit(event){
     event.preventDefault();
     var form = event.target;
-
-    // SCAM test
-    if (form.robot.value.length) { return false; }
-
     $.ajax({
-        url: "https://formspree.io/ideros21@gmail.com",
-        method: "POST",
-        data: {
-          _subject: form.name.value + ' sent you a message from portfolio',
+      url: "https://formspree.io/ideros21@gmail.com",
+      method: "POST",
+      dataType: "json",
+      data: {
+        _subject: 'tu as recu un courriel de ton site',
           email: form.email.value,
           message: form.message.value
-        },
-        dataType: "json",
-        error: onSubmitError,
-        success: onSubmitSuccess
-    });
-
-    // Show loader
-
+      },
+      
+  }).then((value) => {
+    onSubmitSuccess();
+  }).catch((value)=>{
+    onSubmitError();
+  });
     function onSubmitError(){
-      // Show error message, allow trying again
+      $('.js-contact-error').show();
     }
 
     function onSubmitSuccess(){
-      // Show success message
+      $('#name').val("");
+      $('#email').val("");
+      $('#message').val("");
+      $('.js-contact-success').show();
+      contactSubmit.prop('disabled', true);
     }
   }
 
   function onContactInput(event){
-    // Disable/enable submit inputs
+    contactSubmit.attr('disabled', !contactMessage.value.length);
   }
-})()
+})();
